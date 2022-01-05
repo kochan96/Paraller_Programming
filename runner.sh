b57c8ae0b1c286cedd3dfb2503e4c6310f16ddfd
Marcin Kochanowski <marcin.kochanowski96@gmail.com>
2022-01-05 20:10:20 +0100

    test-same-matrix

--- runner.sh
+++ runner.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# for((i=10;i<=100;i+=10))
-# do
-#     sizes+=($i);
-# done
+for((i=100;i<=2000;i+=100))
+do
+    sizes+=($i);
+done
 
-file_name_seq=seq-1000-many.csv
-file_name_parallel=parallel-1000-many.csv
+file_name_seq=seq-800-same-matrix.csv
+file_name_parallel=parallel-800-same-matrix.csv
 
 rm -rf ${file_name_seq}
 rm -rf ${file_name_parallel}
 
-for((i=0;i<10;i++))
+for((i=0;i<${#sizes[@]};i++))
 do
-    ./qr_factorization -n 1000 -o ${file_name_seq}
+    ./qr_factorization -n ${sizes[i]} -o ${file_name_seq}
 done
 
-for((i=0;i<10;i++))
-do
-    ./qr_factorization -n 1000 -o ${file_name_parallel} -p
-done
+# for((i=0;i<10;i++))
+# do
+#     ./qr_factorization -n 1000 -o ${file_name_parallel} -p
+# done
