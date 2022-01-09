edc685387f62fe149fd7a46c46c35bd046428fac
Marcin Kochanowski <marcin.kochanowski96@gmail.com>
2022-01-09 10:43:53 +0100

    sprawozdanie

--- runner.sh
+++ runner.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for((i=100;i<=2000;i+=100))
+for((i=100;i<=550;i+=50))
 do
     sizes+=($i);
 done
 
-file_name_seq=seq-800-same-matrix.csv
-file_name_parallel=parallel-800-same-matrix.csv
+file_name_seq=seq-100-1000.csv
+file_name_parallel=parallel-100-1000.csv
 
 rm -rf ${file_name_seq}
 rm -rf ${file_name_parallel}
@@ -16,7 +16,7 @@ do
     ./qr_factorization -n ${sizes[i]} -o ${file_name_seq}
 done
 
-# for((i=0;i<10;i++))
-# do
-#     ./qr_factorization -n 1000 -o ${file_name_parallel} -p
-# done
+for((i=0;i<${#sizes[@]};i++))
+do
+    ./qr_factorization -n ${sizes[i]} -o ${file_name_parallel} -p
+done
