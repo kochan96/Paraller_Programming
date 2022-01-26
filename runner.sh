b7a6c9cbe30d00cf747698bfc0f1755a61130c35
Marcin Kochanowski <marcin.kochanowski96@gmail.com>
2022-01-26 19:38:59 +0100

    update csv

--- runner.sh
+++ runner.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-for((i=50;i<=550;i+=50))
+for((i=10;i<=100;i+=10))
 do
     sizes+=($i);
 done
 
-file_name_mpi=mpi-50-550.csv
+file_name_mpi=mpi-10-100.csv
 
 rm -rf ${file_name_mpi}
 
 for((i=0;i<${#sizes[@]};i++))
 do
-    mpirun -np 4 ./qr_factorization -n ${sizes[i]} -o ${file_name_mpi}
+    mpirun -np 2 ./qr_factorization -n ${sizes[i]} -o ${file_name_mpi}
 done
